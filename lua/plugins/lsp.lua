550ca31712f86411cc96fc8156c8e41aa2142e49
Mauro Infante <maurux01@gmail.com>
2025-08-25 19:38:21 -0500

    lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -181,7 +181,10 @@ return {
   -- DAP UI
   {
     "rcarriga/nvim-dap-ui",
-    dependencies = { "mfussenegger/nvim-dap" },
+    dependencies = { 
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio"
+    },
     config = function()
       require("dapui").setup()
     end,
