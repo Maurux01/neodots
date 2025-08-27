fed92d0317b151ff7f3203d938d8edce434b69a8
Mauro Infante <maurux01@gmail.com>
2025-08-27 14:07:01 -0500

    whickeys

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -183,29 +183,70 @@ return {
     config = function()
       local wk = require("which-key")
       wk.setup({
-        preset = "modern",
-        delay = 300,
-        expand = 1,
-        notify = true,
-        triggers = {
-          { "<auto>", mode = "nxsot" },
+        plugins = {
+          marks = true,
+          registers = true,
+          spelling = {
+            enabled = true,
+            suggestions = 20,
+          },
+          presets = {
+            operators = false,
+            motions = true,
+            text_objects = true,
+            windows = true,
+            nav = true,
+            z = true,
+            g = true,
+          },
+        },
+        operators = { gc = "Comments" },
+        key_labels = {},
+        icons = {
+          breadcrumb = "»",
+          separator = "➜",
+          group = "+",
+        },
+        popup_mappings = {
+          scroll_down = "<c-d>",
+          scroll_up = "<c-u>",
+        },
+        window = {
+          border = "rounded",
+          position = "bottom",
+          margin = { 1, 0, 1, 0 },
+          padding = { 2, 2, 2, 2 },
+          winblend = 0,
+        },
+        layout = {
+          height = { min = 4, max = 25 },
+          width = { min = 20, max = 50 },
+          spacing = 3,
+          align = "left",
+        },
+        ignore_missing = true,
+        hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " },
+        show_help = true,
+        triggers = "auto",
+        triggers_blacklist = {
+          i = { "j", "k" },
+          v = { "j", "k" },
         },
       })
       
-      wk.add({
-        { "<leader>b", group = "buffers" },
-        { "<leader>c", group = "code" },
-        { "<leader>cs", group = "codesnap" },
-        { "<leader>d", group = "debug" },
-        { "<leader>f", group = "find/file" },
-        { "<leader>g", group = "git" },
-        { "<leader>l", group = "lsp" },
-        { "<leader>m", group = "markdown" },
-        { "<leader>q", group = "session" },
-        { "<leader>t", group = "terminal/themes" },
-        { "<leader>u", group = "ui" },
-        { "<leader>w", group = "windows" },
-      })
+      wk.register({
+        b = { name = "buffers" },
+        c = { name = "code" },
+        d = { name = "debug" },
+        f = { name = "find/file" },
+        g = { name = "git" },
+        l = { name = "lsp" },
+        m = { name = "markdown" },
+        q = { name = "session" },
+        t = { name = "terminal/themes" },
+        u = { name = "ui" },
+        w = { name = "windows" },
+      }, { prefix = "<leader>" })
     end,
   },
 
